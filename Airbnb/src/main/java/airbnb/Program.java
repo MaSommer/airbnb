4b3523c7b20ada477a8b0e671c836566de1a165b
MaSommer <martisom@stud.ntnu.no>
2017-03-04 19:35:37 +0100

    classname change

---
+++ Airbnb/src/main/java/airbnb/Program.java
@@ -0,0 +1,18 @@
+package airbnb;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+import java.io.StringReader;
+import java.util.Arrays;
+
+
+public class Program {	
+		
+	
+
+}
