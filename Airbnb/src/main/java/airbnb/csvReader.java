8e353d2ca4485d1a024e6cff7d4f1bc2830dae81
anderhr1 <andersronold@dhcp-10-24-25-6.wlan.ntnu.no>
2017-03-04 11:04:19 +0100

    s

--- Airbnb/src/main/java/airbnb/csvReader.java
+++ Airbnb/src/main/java/airbnb/csvReader.java
@@ -10,19 +10,4 @@ import com.google.common.base.Function;
 
 public class csvReader {
 	
-	public static void main(String[] args) {
-		SparkConf conf = new SparkConf().setAppName("csvReader").setMaster("local");
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		
-		
-        JavaRDD<String> csvFile = sc.textFile("D:\\MOCK_DATA_spark.csv");
-        JavaRDD<String[]> csvData = csvFile.map(new LoadCSVJavaExample());
-        System.out.println("This prints the total count " + csvData.count());
-    }
-
-    public String[] call(String line) throws Exception {
-        CSVReader reader = new CSVReader(new StringReader(line));
-        return reader.readNext();
-    }
 }
