138cc5a27b81d6915cf3fefc736f525c4520d953
anderhr1 <andersronold@dhcp-10-24-25-6.wlan.ntnu.no>
2017-03-04 14:19:30 +0100

    heisann

--- Airbnb/src/main/java/airbnb/Main.java
+++ Airbnb/src/main/java/airbnb/Main.java
@@ -31,33 +31,29 @@ public class Main {
 //		System.out.println(priceIndex);
 //		ArrayList<String> priceList = new ArrayList<String>();
 		
-
-		SparkConf conf = new SparkConf()
-		.setAppName("SF Salaries")
-		.setMaster("local[*]")
-		;
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaRDD<String> inputRDD = sc.textFile("target/listings_us.csv");
-		
-		JavaRDD<String[]> result = inputRDD.map(new Function<String, String[]>() {
-			public String[] call(String s) {
-//				System.out.println(s);
-				String[] cityAndPrice = new String[2];
-				String[] list = s.split("\t");
-				cityAndPrice[0] = list[15];
-				cityAndPrice[1] = list[65];
-				System.out.println(Arrays.toString(cityAndPrice));
-				
-				return cityAndPrice;
-			} 
-		});
-		
-		System.out.println(result.count());
-		
-
 	}
 	
 	public static void main(String[] args) {
 		new Main();
+		SparkConf conf = new SparkConf()
+				.setAppName("SF Salaries")
+				.setMaster("local[*]")
+				;
+				JavaSparkContext sc = new JavaSparkContext(conf);
+				JavaRDD<String> inputRDD = sc.textFile("target/listings_us.csv");
+				
+				JavaRDD<String[]> result = inputRDD.map(new Function<String, String[]>() {
+					public String[] call(String s) {
+//						System.out.println(s);
+						String[] cityAndPrice = new String[2];
+						String[] list = s.split("\t");
+						cityAndPrice[0] = list[15];
+						cityAndPrice[1] = list[65];
+						
+						return cityAndPrice;
+					} 
+				});
+				
+				System.out.println(result.reduce(new ));
+			}
 	}	
-}
