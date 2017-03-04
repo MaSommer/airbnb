c8e1177f200f7582debf6aac1cc9cec701b88f52
MaSommer <martisom@stud.ntnu.no>
2017-03-04 11:34:31 +0100

    main

--- Airbnb/src/main/java/airbnb/Main.java
+++ Airbnb/src/main/java/airbnb/Main.java
@@ -4,15 +4,64 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.*;
 
 import java.io.StringReader;
+import java.util.Arrays;
 
 public class Main {
 	
 	
 	public static void main(String[] args) {
+		
+		SparkConf conf = new SparkConf()
+        .setAppName("SF Salaries")
+        .setMaster("local[*]")
+        ;
+		JavaSparkContext sc = new JavaSparkContext(conf);
+//		JavaRDD<String> lines = sc.text	File("target/calendar_us.csv");
+		
+		JavaRDD<String> inputRDD = sc.textFile("target/calendar_us.csv");
+		JavaRDD<String> errorsRDD = inputRDD.filter( new Function<String, Boolean>() {
+			public Boolean call(String x) { return x.contains("error"); }
+		});
+		errorsRDD = inputRDD.filter( new Function<String, Boolean>() {
+			public Boolean call(String x) { return x.contains("error"); }
+		});
+		
+		JavaRDD<String> 	warningsRDD = inputRDD.filter( new Function<String, Boolean>() {
+			public Boolean call(String x) { return x.contains("warning"); }
+		});
+		JavaRDD<String> badLinesRDD = errorsRDD.union(warningsRDD);
+		System.out.println("Input had " + badLinesRDD.count() + " concerning lines");
+		System.out.println("Here are 10 examples:");
+		for (String line: badLinesRDD.take(10)) {
+		  System.out.println(line);
+		}
+
+
+		
+//		SparkConf conf = new SparkConf()
+//        .setAppName("SF Salaries")
+//        .setMaster("local[*]")
+//        ;
+//		JavaSparkContext javaSparkContext = new JavaSparkContext(conf);
+//		SQLContext sqlContext = new SQLContext(javaSparkContext);
+//		System.out.println("1");
+//		
+//		DataFrame dataFrame = sqlContext.read()
+//			    .format("com.databricks.spark.csv")
+//			    .option("inferSchema", "true")
+//			    .option("header", "true")
+//			    .load("target/calendar_us.csv");
+//		
+//		System.out.println("2");
+//		System.out.println(dataFrame.col("listing_id\tdate\tavailable").toString());
+
+
 //	    SparkConf sparkConf = new SparkConf().setAppName("Pussy&Alcohol");
 //	    JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+	    
 //
 //	    // Read the source file
 //	    JavaRDD<String> input = sparkContext.textFile("target/calendar_us.csv");
