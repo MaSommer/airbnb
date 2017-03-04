3262e915ae0ff93da3d2e0b93fac1cc887b5175c
anderhr1 <andersronold@dhcp-10-24-25-6.wlan.ntnu.no>
2017-03-04 11:16:15 +0100

    sd

--- Airbnb/src/main/java/airbnb/Main.java
+++ Airbnb/src/main/java/airbnb/Main.java
@@ -1,21 +1,12 @@
 package airbnb;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 
 import java.io.StringReader;
-<<<<<<< HEAD
-=======
+
 
 public class Main {
 	
-	
-	
-	
->>>>>>> master
-
-public class Main {
-	
+	public static void main(String[] args) {
+		System.out.println("hei");
+	}
 }
