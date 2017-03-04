cb60707ec4fa5a15dd33c3ff15db8613931c0239
MaSommer <martisom@stud.ntnu.no>
2017-03-04 19:41:58 +0100

    g

--- Airbnb/src/main/java/airbnb/Main.java
+++ Airbnb/src/main/java/airbnb/Main.java
@@ -5,7 +5,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 import java.io.StringReader;
 import java.util.Arrays;
